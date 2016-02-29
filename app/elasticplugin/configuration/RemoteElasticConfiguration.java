5bdcef54ce37435cfc210c25ef63a7c8f2b4920e
Johannes Unterstein <unterstein@me.com>
2016-02-29 10:55:21 +0100

    removed deprecated annotation information

--- app/elasticplugin/configuration/RemoteElasticConfiguration.java
+++ app/elasticplugin/configuration/RemoteElasticConfiguration.java
@@ -9,7 +9,6 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.repository.cdi.ElasticsearchRepositoryBean;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import java.net.InetAddress;
